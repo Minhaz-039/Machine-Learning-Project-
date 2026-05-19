3deeaa2f5eb7b962a2f328347f0a48aa900b7b2f
Minhaz-039 <minhazreza3243@gmail.com>
2026-05-19 09:27:17 +0600

    URL checked on frontend

--- Frontend/vite-project/src/App.jsx
+++ Frontend/vite-project/src/App.jsx
@@ -33,11 +33,14 @@ function App() {
     setSummaryInputs(null);
 
     try {
-      const response = await fetch("http://localhost:5000/predict", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(formData),
-      });
+      const response = await fetch(
+        "https://machine-learning-project-1-wwky.onrender.com",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(formData),
+        },
+      );
 
       if (!response.ok) throw new Error("Failed to fetch prediction");
 
